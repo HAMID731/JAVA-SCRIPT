66ac82e897c70fc3846dd659517f21b7a067434f
HAMID <abdulhamidabari652@gmail.com>
2025-02-04 15:36:04 +0100

    Array

---
+++ n.js
@@ -0,0 +1,10 @@
+const arrayOfNumber = new Array()
+
+arrayOfNumber.push(1)
+arrayOfNumber.push(3)
+arrayOfNumber.push(4)
+
+let firstNumber = "chinedu";
+
+let newArray = Array.from(firstNumber);
+console.log(newArray);
